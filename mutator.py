c94ac7539e3dc82978887239002155540557a42b
tomas.kindahl@bahnhof.se <tomas.kindahl@bahnhof.se>
2025-10-03 05:38:21 +0200

    fixme mutator

--- mutator.py
+++ mutator.py
@@ -2,14 +2,19 @@ import requests
 import random
 import sys
 import os
+import time
 
-def main(url):
-    humid = random.randint(60,90)
-    temp = random.randint(0,10)
-    D = str({"humid": humid, "station": 666, "temp": temp}).encode('utf-8')
-    H = {'Content-Type': "application/json",
-         'Accept': "application/vnd.pagerduty+json;version=2"}
-    P = requests.put(url+'/reg', data=D, header=H)
+def main(url, station):
+    while True:
+        humid = random.randint(60,90)
+        temp = random.randint(0,10)
+        D = f'{{"humid": "{humid}", "station": "{station}", "temp": "{temp}"}}'.encode('utf-8')
+        print(D)
+        H = {'Content-Type': "application/json",
+             'Accept': "application/vnd.pagerduty+json;version=2"}
+        P = requests.put(url+'/reg', data=D, headers=H)
+        print(P.status_code)
+        time.sleep(10)
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
