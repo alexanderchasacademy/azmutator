d914e91afd5e1de4ca6abf323412c80aae120b4f
tomas.kindahl@bahnhof.se <tomas.kindahl@bahnhof.se>
2025-10-03 05:09:29 +0200

    argv[1] is intended to be url

--- mutator.py
+++ mutator.py
@@ -1,8 +1,9 @@
 import requests
 import random
+import sys
 import os
 
-def main():
+def main(url):
     humid = random.randint(60,90)
     temp = random.randint(0,10)
     D = str({"humid": humid, "station": 666, "temp": temp}).encode('utf-8')
@@ -11,4 +12,4 @@ def main():
     P = requests.put(url+'/reg', data=D, header=H)
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
