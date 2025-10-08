1293d970548e0b5a8f85d64ad299ae162cf65fbf
alexanderchasacademy <alexander.arvedson@chasacademy.se>
2025-10-08 11:33:55 +0200

    Added debug prints to verify Azure run

--- mutator.py
+++ mutator.py
@@ -1,20 +1,30 @@
 import requests
 import random
 import sys
-import os
 import time
 
 def main(url, station):
+    print("Mutator started!")  # <-- Ny rad
     while True:
-        humid = random.randint(60,90)
-        temp = random.randint(0,10)
-        D = f'{{"humid": "{humid}", "station": "{station}", "temp": "{temp}"}}'.encode('utf-8')
-        print(D)
-        H = {'Content-Type': "application/json",
-             'Accept': "application/vnd.pagerduty+json;version=2"}
-        P = requests.put(url+'/reg', data=D, headers=H)
-        print(P.status_code)
-        time.sleep(10)
+        humid = random.randint(60, 90)
+        temp = random.randint(0, 10)
+        data = {
+            "humid": str(humid),
+            "station": str(station),
+            "temp": str(temp)
+        }
+        print("Sending data:", data)  # <-- Ny rad
+        try:
+            r = requests.put(url, json=data)
+            print("Response:", r.status_code, r.text)  # <-- Ny rad
+        except Exception as e:
+            print("Error:", e)
+        time.sleep(60)  # skicka var 60:e sekund
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    if len(sys.argv) < 2:
+        print("Usage: python mutator.py <url> [station]")
+        sys.exit(1)
+    url = sys.argv[1]
+    station = sys.argv[2] if len(sys.argv) > 2 else "99"
+    main(url, station)
